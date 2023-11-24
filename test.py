43d3a1418879982d268a6e0878501cc55cd68809
darf53 <none>
2023-11-24 09:42:39 +0100

    test for x vertical created

--- test.py
+++ test.py
@@ -7,5 +7,9 @@ class TestBoard(unittest.TestCase):
     def test_board(self):
         self.assertEqual(print_board([["","",""],["","",""],["","",""]]), " | | \n-+-+-\n | | \n-+-+-\n | | \n", "Should be an empty TicTacToe board")
 
+    def test_XwonVert(self):
+        self.assertTrue(test_board([["X","",""],["X","O",""],["X","","O"]]))
+
+
 if __name__ == '__main__':
     unittest.main()
